58b256c25c5e3feab8c32f9e12738659a5f7fe94
allen <allenmuhani@gmail.com>
2025-04-06 20:26:42 +0300

    added reducer and sagas to the root

--- app/redux/root.reducer.ts
+++ app/redux/root.reducer.ts
@@ -2,4 +2,4 @@ import { combineReducers } from "@reduxjs/toolkit";
 import { mintingReducer } from "~/features/minting_certificate/store";
 
 // instead of defining the reducers in the reducer field of configureStore, combine them here:
-export const rootReducer = combineReducers(mintingReducer);
+export const rootReducer = combineReducers({ minting: mintingReducer });
--- app/redux/root.saga.ts
+++ app/redux/root.saga.ts
@@ -1,2 +1,6 @@
+import { spawn } from "redux-saga/effects";
+import { mint_sagas } from "~/features/minting_certificate/sagas";
+
 export function* root_saga() {
+  yield spawn(mint_sagas);
 }
